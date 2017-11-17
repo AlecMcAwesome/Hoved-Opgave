aed7bc538ff97056865306da5c3054862c7730b7
FrederikBP <fbp@stromworks.com>
2017-11-17 12:45:17 +0100

    Admin functions now available

--- src/AppBundle/Controller/ProfileController.php
+++ src/AppBundle/Controller/ProfileController.php
@@ -2,12 +2,18 @@
 
 namespace AppBundle\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class ProfileController extends Controller
 {
-    public function indexAction($name)
+    /**
+     * @Route("/profile", name="profilepage")
+     */
+
+    public function indexAction(Response $response)
     {
-        return $this->render('', array('name' => $name));
+        return $this->render('@FOSUser/Profile/show.html.twig');
     }
 }
--- src/AppBundle/EventListener/RedirectAfterRegistration.php
+++ src/AppBundle/EventListener/RedirectAfterRegistration.php
@@ -6,7 +6,54 @@
  * Time: 13.30
  */
 
-class RedirectAfterRegistration
-{
+namespace AppBundle\EventListener;
 
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use FOS\UserBundle\FOSUserEvents;
+use FOS\UserBundle\Event\FormEvent;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
+
+
+class RedirectAfterRegistration implements EventSubscriberInterface
+{
+    /**
+     * @var RouterInterface
+     */
+    private $router;
+
+    public function __construct(RouterInterface $router){
+        $this->router = $router;
+    }
+
+    /**
+     * Returns an array of event names this subscriber wants to listen to.
+     *
+     * The array keys are event names and the value can be:
+     *
+     *  * The method name to call (priority defaults to 0)
+     *  * An array composed of the method name to call and the priority
+     *  * An array of arrays composed of the method names to call and respective
+     *    priorities, or 0 if unset
+     *
+     * For instance:
+     *
+     *  * array('eventName' => 'methodName')
+     *  * array('eventName' => array('methodName', $priority))
+     *  * array('eventName' => array(array('methodName1', $priority), array('methodName2')))
+     *
+     * @return array The event names to listen to
+     */
+    public static function getSubscribedEvents(){
+        return [
+            FOSUserEvents::REGISTRATION_SUCCESS => 'onRegistrationSuccess'
+        ];
+    }
+
+    public function onRegistrationSuccess(FormEvent $event){
+        $url = $this->router->generate('homepage');
+        $response = new RedirectResponse($url);
+        $event->setResponse($response);
+    }
 }
