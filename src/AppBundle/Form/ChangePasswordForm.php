04d0375e41350a503a4372b3aeff0e4625fbe57f
FrederikBP <fbp@stromworks.com>
2017-11-19 22:06:49 +0100

    admin classes

---
+++ src/AppBundle/Form/ChangePasswordForm.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ChangePasswordForm extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+
+    }
+
+    public function getBlockPrefix()
+    {
+        return 'app_bundle_change_password_form';
+    }
+}
